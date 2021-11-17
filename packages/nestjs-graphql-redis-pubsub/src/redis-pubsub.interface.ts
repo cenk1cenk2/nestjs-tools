1fbab6ecce6b22099505756c03c63d9b261bf75d
Cenk Kılıç <cenk@kilic.dev>
2021-11-17 14:42:00 +0100

    fix: type error

--- packages/nestjs-graphql-redis-pubsub/src/redis-pubsub.interface.ts
+++ packages/nestjs-graphql-redis-pubsub/src/redis-pubsub.interface.ts
@@ -1,7 +1,7 @@
 import { RedisPubSub } from 'graphql-redis-subscriptions'
 import { PubSubRedisOptions } from 'graphql-redis-subscriptions/dist/redis-pubsub'
 
-export type RedisPubSubService = RedisPubSub
+export type RedisPubSubProvider = RedisPubSub
 
 export interface RedisPubSubModuleOptions {
   global?: boolean
