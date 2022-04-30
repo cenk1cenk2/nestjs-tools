a36fb74cc4776fc641c5b0208c501c5bd91d5541
Cenk Kılıç <cenk@kilic.dev>
2022-05-01 00:25:00 +0200

    docs: update documentation

--- scripts/link-packages-to-workspace.sh
+++ scripts/link-packages-to-workspace.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
+#!/bin/bash
+
 if [ -z "$1" ]; then
-	echo "Parameter should be link | unlink"
+	echo "First parameter should be link | unlink"
 	exit 127
 fi
 
 if [ -z "$2" ]; then
-	echo "Parameter should be the pwd of created workspace."
+	echo "Second parameter should be the pwd of created workspace."
 	exit 127
 fi
 
 [ ! -d "$2" ] && echo "Directory $2 not found."
 
+PACKAGES=packages/*/package.json
+
 echo "Running against directory: $2"
-cd $1
 
-declare -a PACKAGES=(@cenk1cenk2/nestjs-emitter @cenk1cenk2/nestjs-transactions @cenk1cenk2/nestjs-utils)
-
-for i in "${PACKAGES[@]}"; do
-	yarn $1 $i
+for PACKAGE in "${PACKAGES[@]}"; do
+	PKG_NAME=$(cat $PACKAGE | jq -r .name)
+	(cd "$2" && yarn $1 $PKG_NAME)
 done
--- scripts/link-packages.sh
+++ scripts/link-packages.sh
@@ -4,15 +4,15 @@ echo "Register all packages globally for development."
 PACKAGES=packages/*
 
 if [ -z "$1" ]; then
-  echo "Give out a command: link | unlink"
+	echo "Give out a command: link | unlink"
 fi
 
 for PACKAGE in $PACKAGES; do
-  echo "Package: $PACKAGE"
+	echo "Package: $PACKAGE"
 
-  if [ "$1" == "link" ]; then
-    (cd $PACKAGE && yarn link)
-  elif [ "$1" == "unlink" ]; then
-    (cd $PACKAGE && yarn unlink)
-  fi
+	if [ "$1" == "link" ]; then
+		(cd $PACKAGE && yarn link)
+	elif [ "$1" == "unlink" ]; then
+		(cd $PACKAGE && yarn unlink)
+	fi
 done
