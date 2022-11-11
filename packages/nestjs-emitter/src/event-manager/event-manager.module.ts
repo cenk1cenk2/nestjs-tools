beaa6a9f952b72a946fe919039625c07950aeb7a
Cenk Kılıç <cenk@kilic.dev>
2022-11-11 12:06:44 +0100

    fix: update event emitter injection

--- packages/nestjs-emitter/src/event-manager/event-manager.module.ts
+++ packages/nestjs-emitter/src/event-manager/event-manager.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from '@nestjs/common'
+import { EventEmitter2 } from '@nestjs/event-emitter'
 
 import { EventManager } from './event-manager.service'
 
 @Global()
 @Module({
-  providers: [ EventManager ],
+  providers: [ EventManager, EventEmitter2 ],
   exports: [ EventManager ]
 })
 export class EventManagerModule {}
